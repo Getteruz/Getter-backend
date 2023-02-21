c3daf03a04dc7c89d1bf8e14ac3b24cd50c4d64d
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-02-21 21:41:31 +0500

    feat: added typeorm and swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'warn', 'error'],
+  });
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Getter uz')
+    .setDescription('Getter API description')
+    .setVersion('0.2')
+    .addBearerAuth()
+    .addCookieAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
